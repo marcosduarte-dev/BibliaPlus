12243097c617b4e162f7235e0f81bf36c8126422
Marcos Duarte <pe.marcos30@gmail.com>
2024-04-18 18:54:00 -0300

    Adicionando indice

---
+++ src/components/Indice.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { IndiceService } from "../service/indice";
+import Tree from "primevue/tree";
+
+const nodes = ref();
+const selectedKey = ref();
+
+onMounted(() => {
+  IndiceService.getIndiceBiblia().then((data) => (nodes.value = data));
+});
+
+const onNodeSelect = (node: any) => {
+  console.log(node.label + " selected");
+};
+
+const onNodeUnselect = (node: any) => {
+  console.log(node.label + " unselected");
+};
+</script>
+
+<template>
+  <Tree
+    v-model:selectionKeys="selectedKey"
+    :value="nodes"
+    selectionMode="single"
+    class="w-full md:w-30rem"
+  ></Tree>
+</template>
